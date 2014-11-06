c4d3a03e9dd4b53db542ef4d7503eb4ac465bbe8
Derrick Zwickl <zwickl@email.arizona.edu>
2014-11-06 13:01:30 -0700

    better example script

--- examples/quartetExampleMasterScript.sh
+++ examples/quartetExampleMasterScript.sh
@@ -7,10 +7,13 @@ SCRIPTDIR=../scripts/
 #pipe a list of bootstrap treefiles to dendropyScoreTriples.py script, read a quartet file and summarize 
 #the trees, outputting tabular summary files into the treeset1 and treeset2 directories
 ls inputAndOutput/treeset1/*.blink.00*.boot.tre | $SCRIPTDIR/dendropyScoreTriples.py -o inputAndOutput/treeset1 --quartet-file quartetList
-if [ "$?" -ne "0" ];then echo "Problem running examples!"; fi
+if [ "$?" -ne "0" ];then echo "Problem running examples!"; quit; fi
 
 ls inputAndOutput/treeset2/*.blink.00*.boot.tre | $SCRIPTDIR/dendropyScoreTriples.py -o inputAndOutput/treeset2 --quartet-file quartetList
-if [ "$?" -ne "0" ];then echo "Problem running examples!"; fi
+if [ "$?" -ne "0" ];then echo "Problem running examples!"; quit; fi
+
+exit
+
 mkdir -p figures
 
 #pass the corresponding quartet output files from each of the two output directories to the plotting
@@ -18,12 +21,13 @@ mkdir -p figures
 for QUART in nivara.punctata.barthii.glaberrimaM.dat  sativaj.punctata.barthii.glaberrimaM.dat
 do
     #plot cumulative figures for each treatment
-    $SCRIPTDIR/cumulativeAndPieFigure.py -i inputAndOutput/treeset1/$QUART inputAndOutput/treeset2/$QUART -o figures/$QUART.cumulative.pdf --columns 2 \
+    #$SCRIPTDIR/cumulativeAndPieFigure.py -i inputAndOutput/treeset1/$QUART inputAndOutput/treeset2/$QUART -o figures/$QUART.cumulative.pdf --columns 2 \
+    $SCRIPTDIR/cumulativeAndPieFigure.py -i inputAndOutput/treeset1/$QUART -o figures/$QUART.cumulative.pdf --columns 2 \
         --titles "Treatment 1" "Treatment 2"
-    if [ "$?" -ne "0" ];then echo "Problem running examples!"; fi
+    if [ "$?" -ne "0" ];then echo "Problem running examples!"; quit; fi
 
     #plot flux diagrams of comparison of pairs of treatments
     ../scripts/calculateFlux.py inputAndOutput/treeset1/$QUART inputAndOutput/treeset2/$QUART | ../scripts/plotFlux.py -o figures/$QUART.flux.pdf
-    if [ "$?" -ne "0" ];then echo "Problem running examples!"; fi
+    if [ "$?" -ne "0" ];then echo "Problem running examples!"; quit; fi
 done
 
